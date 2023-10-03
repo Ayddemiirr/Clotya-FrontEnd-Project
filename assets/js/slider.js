449d16e4566d266007d972765c89e970e817f92a
Ayddemiirr <aydamirkr@code.edu.az>
2023-10-03 18:25:52 +0400

    home fixed

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -11,9 +11,11 @@ $(document).ready(function () {
     $('.featured-products').slick({
         prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
         nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
-        dots: true,
         infinite: true,
+        dots: true,
         slidesToShow: 4,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        variableWidth: true,
     });
 });
