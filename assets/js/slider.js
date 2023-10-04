c040da4149bba9e670ec8d5bf14af854acc8f55e
Ayddemiirr <aydamirkr@code.edu.az>
2023-10-04 20:35:58 +0400

    home fixed

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -19,3 +19,16 @@ $(document).ready(function () {
         variableWidth: true,
     });
 });
+
+$(document).ready(function () { 
+    $('.featured-products-wrapper').slick({
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
+        infinite: true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        variableWidth: true,
+    });
+});
